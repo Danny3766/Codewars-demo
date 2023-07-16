adb527e9c78291268eec9bf604500293d2df9fd8
Ting <woodming9889@gmail.com>
2023-07-16 11:10:20 +0800

    add #11 JS training challenge

--- javascript-training-challenge/Training JS #10 loop statement --for.js
+++ javascript-training-challenge/Training JS #10 loop statement --for.js
@@ -60,3 +60,16 @@ function pickIt(arr) {
 
     return [odd, even];
 }
+
+// other my favorite solution
+function pickIt(arr) {
+
+    let odd = []
+    let even = []
+
+    for (var x of arr) {
+        ((x % 2) ? odd : even).push(x)
+    }
+
+    return [odd, even]
+}
